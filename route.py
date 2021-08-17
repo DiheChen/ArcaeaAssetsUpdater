3a3e3e523c34e66e21341151bdc4164689b1305d
Chendihe4975 <Chendihe4975@gmail.com>
2021-08-18 00:22:53 +0800

    fix bugs

--- route.py
+++ route.py
@@ -1,7 +1,7 @@
 """
  - Author: DiheChen
  - Date: 2021-08-15 00:13:33
- - LastEditTime: 2021-08-15 04:54:58
+ - LastEditTime: 2021-08-18 00:22:19
  - LastEditors: DiheChen
  - Description: None
  - GitHub: https://github.com/Chendihe4975
@@ -31,6 +31,7 @@ async def _():
     song_dict = dict()
     for song in listdir(songs_dir):
         if path.isdir(path.join(songs_dir, song)):
-            song_dict[song.replace("dl_", "")] = urljoin(
-                Config.url, pathname2url(path.join("assets", "songs", song.replace("dl_", ""), "base.jpg")))
+            if path.exists(path.join(songs_dir, song, "base.jpg")):
+                song_dict[song.replace("dl_", "")] = urljoin(Config.url, pathname2url(
+                    path.join("assets", "songs", song.replace("dl_", ""), "base.jpg")))
     return song_dict
